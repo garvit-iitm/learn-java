8d58cb0e9c16a5ef1765b0ff47d77278d08b7698
garvit singh bhatnagar <23f1001875@ds.study.iitm.ac.in>
2026-06-16 22:51:04 +0530

    learn how to take input using Scanner object

---
+++ src/Garvit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Garvit {
+    public static void main(String[] args) 
+    {
+        Scanner ob = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = ob.nextInt();
+
+        System.out.println("Your entered number is: " + num);
+        
+    }
+}
